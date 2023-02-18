5d2543519080815c1fcc9ce9a1e27a792dd88f20
alphaClass6143 <alphaClass@web.de>
2023-02-18 23:02:32 +0100

    Updated structure and fixed link bugs

--- app/app.py
+++ app/app.py
@@ -1,25 +1,45 @@
+'''
+Main flask setup
+'''
 from flask import Flask, render_template
 
+# Flask setup
 app = Flask(__name__)
 app = Flask(__name__, static_folder='assets')
 
-# Please create routes
+# Static pages
 @app.route('/')
 def index():
+    '''
+    Renders index page
+    '''
     return render_template('index.html')
 
 @app.route('/about-us')
 def aboutUs():
-    return render_template('about-us.html')
+    '''
+    Renders about us page
+    '''
+    return render_template('about_us.html')
+
+@app.route('/contact-us')
+def contactUs():
+    return render_template('contact_us.html')
 
 @app.route('/search')
 def search():
+    '''
+    Renders search result
+    '''
     return render_template('search_result.html')
 
 @app.route('/charity/<slug>')
 def charity(slug):
+    '''
+    Renders a given charity
+    '''
     # Get charity here
-    charity = {
+    charityobj = {
         "name": "Humane Society International",
         "goal": "To protect animals and their habitats worldwide.",
         "origin_location": "USA",
@@ -33,29 +53,36 @@ def charity(slug):
         "keywords": ["animal welfare", "wildlife conservation"],
         "slug": slug
     }
-    return render_template('charity.html', charity=charity)
+    return render_template('charity.html', charity=charityobj)
 
-@app.route('/contact-us')
-def contactUs():
-    return render_template('contact-us.html')
-
+# News articles
 @app.route('/article-1')
 def article1():
+    '''
+    Renders news article 1
+    '''
     return render_template('article_1.html')
 
 @app.route('/article-2')
 def article2():
+    '''
+    Renders news article 2
+    '''
     return render_template('article_2.html')
 
 @app.route('/article-3')
 def article3():
+    '''
+    Renders news article 3
+    '''
     return render_template('article_3.html')
-
 
 @app.errorhandler(404)
 def page_not_found(e):
+    '''
+    Renders 404 page
+    '''
     return render_template('404.html'), 404
-
 
 if __name__ == '__main__':
     app.run(debug=True)
