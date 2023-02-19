10fb7c5b16dd2e548f954f6dfcce9ae513072c63
SamSswi <sadovoisamuel7@gmail.com>
2023-02-19 00:26:44 +0000

    Add functionality to app.py to read json file and render a given charity

--- app/app.py
+++ app/app.py
@@ -1,11 +1,13 @@
 '''
 Main flask setup
 '''
+import json
 from flask import Flask, render_template
 
 # Flask setup
-app = Flask(__name__)
+
 app = Flask(__name__, static_folder='assets')
+
 
 # Static pages
 @app.route('/')
@@ -15,45 +17,68 @@ def index():
     '''
     return render_template('index.html')
 
+
 @app.route('/about-us')
 def aboutUs():
     '''
     Renders about us page
     '''
-    return render_template('about_us.html')
+    return render_template('about_us.html', page_title='About Us')
+
+
+@app.route('/charity')
+def charities():
+    '''
+    Renders charities page, reads the data from charities.json
+    '''
+    data = []
+    with open("../charities.json", "r") as json_data:
+        data = json.load(json_data)
+    return render_template('charity.html', page_title='Charity', charities=data)
+
 
 @app.route('/contact-us')
 def contactUs():
-    return render_template('contact_us.html')
+    return render_template('contact_us.html', page_title='Contact Us')
+
 
 @app.route('/search')
 def search():
     '''
     Renders search result
     '''
-    return render_template('search_result.html')
+    return render_template('search_result.html', page_title='Search')
 
-@app.route('/charity/<slug>')
-def charity(slug):
+
+@app.route('/charity/<company_name>')
+def charity(company_name):
     '''
     Renders a given charity
     '''
     # Get charity here
-    charityobj = {
-        "name": "Humane Society International",
-        "goal": "To protect animals and their habitats worldwide.",
-        "origin_location": "USA",
-        "region_served": "Global",
-        "founder": "Fred Myers and Helen Jones",
-        "year_founded": 1954,
-        "annual_revenue": "$200 million",
-        "wikilink": "https://en.wikipedia.org/wiki/Humane_Society_International",
-        "website_link": "https://www.hsi.org/",
-        "category": "Animals",
-        "keywords": ["animal welfare", "wildlife conservation"],
-        "slug": slug
-    }
-    return render_template('charity.html', charity=charityobj)
+
+    # charityobj = {
+    #     "name": "Humane Society International",
+    #     "goal": "To protect animals and their habitats worldwide.",
+    #     "origin_location": "USA",
+    #     "region_served": "Global",
+    #     "founder": "Fred Myers and Helen Jones",
+    #     "year_founded": 1954,
+    #     "annual_revenue": "$200 million",
+    #     "wikilink": "https://en.wikipedia.org/wiki/Humane_Society_International",
+    #     "website_link": "https://www.hsi.org/",
+    #     "category": "Animals",
+    #     "keywords": ["animal welfare", "wildlife conservation"],
+    #     "slug": slug
+    # }
+    company = {}
+    with open("../charities.json", "r") as json_data:
+        data = json.load(json_data)
+        for object in data:
+            if object["url"] == company_name:
+                company = object
+    return "<h2>" + company["charity_name"] + "</h2>"
+
 
 # News articles
 @app.route('/article-1')
@@ -61,21 +86,24 @@ def article1():
     '''
     Renders news article 1
     '''
-    return render_template('article_1.html')
+    return render_template('article_1.html', page_title='Article 1')
+
 
 @app.route('/article-2')
 def article2():
     '''
     Renders news article 2
     '''
-    return render_template('article_2.html')
+    return render_template('article_2.html', page_title='Article 2')
+
 
 @app.route('/article-3')
 def article3():
     '''
     Renders news article 3
     '''
-    return render_template('article_3.html')
+    return render_template('article_3.html', page_title='Article 3')
+
 
 @app.errorhandler(404)
 def page_not_found(e):
@@ -83,6 +111,7 @@ def page_not_found(e):
     Renders 404 page
     '''
     return render_template('404.html'), 404
+
 
 if __name__ == '__main__':
     app.run(debug=True)
